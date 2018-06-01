e3316ab5598cb9e43fd2e2a35684e395056598cb
Andrei Chira <andreichira777@gmail.com>
2018-06-01 17:13:36 +0300

    added prism css

--- functions.php
+++ functions.php
@@ -420,3 +420,26 @@ function custom_do_header_search_form() {
         $button
     );
 }
+
+// Function to add prism.css and prism.js to the site
+
+function add_prism() {
+// Register prism.css file
+
+wp_register_style(
+'prismCSS', // handle name for the style so we can register, de-register, etc.
+get_stylesheet_directory_uri() . '/prism.css' // location of the prism.css file
+);
+
+// Register prism.js file
+wp_register_script(
+'prismJS', // handle name for the script so we can register, de-register, etc.
+get_stylesheet_directory_uri() . '/js/prism.js' // location of the prism.js file
+);
+
+// Enqueue the registered style and script files
+wp_enqueue_style('prismCSS');
+wp_enqueue_script('prismJS');
+}
+
+add_action('wp_enqueue_scripts', 'add_prism');
