3233e34ae72c9514b98b55c546ec838753c0ffd1
Andrei Chira <andreichira777@gmail.com>
2018-06-09 19:43:00 +0300

    ionicons slashed

--- functions.php
+++ functions.php
@@ -60,7 +60,8 @@ add_action( 'wp_enqueue_scripts', 'genesis_sample_enqueue_scripts_styles' );
 function genesis_sample_enqueue_scripts_styles() {
 
 	wp_enqueue_style(
-    'ionicons', 'https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css',
+    'ionicons',
+		get_stylesheet_directory_uri() . "/ionicons.css",
     array(),
     CHILD_THEME_VERSION
 );
